952cf76c01dd4561ad9abffdaafc86f8e1b5e7e8
Fernando Lopez <misterquestions.mx@gmail.com>
2021-10-08 23:29:44 -0500

    feat: add graphql and config packages for nestjs

--- apps/movies-api/src/app/app.module.ts
+++ apps/movies-api/src/app/app.module.ts
@@ -1,10 +1,31 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import path from 'path';
+
+import { MoviesModule } from './modules/movies/movies.module';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    GraphQLModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        const isProduction = configService.get('NODE_ENV') === 'production';
+        
+        return {
+          debug: !isProduction,
+          playground: !isProduction,
+          autoSchemaFile: path.join(__dirname, 'src/schema.gql'),
+        };
+      },
+    }),
+    MoviesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
